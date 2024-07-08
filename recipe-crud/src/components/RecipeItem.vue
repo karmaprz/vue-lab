987986c6231a215d9573745e75324f3967dfc422
Carmen Perez <carmenp.gomez99@gmail.com>
2024-07-08 11:01:57 +0200

    db

--- recipe-crud/src/components/RecipeItem.vue
+++ recipe-crud/src/components/RecipeItem.vue
@@ -11,7 +11,42 @@ defineProps({
 </script>
 
 <template>
-<div>
-    <div>{{ recipe.title }}</div>
-</div>
+  <div class="recipe-wrapper">
+      <h2>{{ recipe.title }}</h2>
+      <p>Hearty lentil soup with carrots, celery, and spices, perfect for a cozy meal.</p>
+      <span class="buttons-wrapper">
+        <button>Delete recipe</button> | 
+        <button>Edit recipe</button>
+      </span>
+  </div>
 </template>
+
+<style scoped>
+.recipe-wrapper {
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  margin: 20px;
+  border-radius: 5px;
+  border: solid 1px black;
+  padding: 15px;
+}
+
+.buttons-wrapper > button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  padding: 0; 
+  margin: 0;
+  border: none;
+  background: none;
+  font: inherit;
+  color: inherit;
+  line-height: normal;
+  cursor: pointer;
+  outline: none;
+  text-decoration: underline;
+  text-decoration-color: rgb(167, 0, 189);
+}
+
+</style>
